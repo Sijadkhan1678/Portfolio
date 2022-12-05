ad3e2d9f9bd42d639dec6300c744739ba6020488
sijadkhan1678 <sijadkhan1678@gmail.com>
2022-12-05 09:58:22 +0500

    Added Content in the Projects array

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
- const Projects = () => {
+const Projects = () => {
+
+  const projects = [{ title: "Covid19-Tracker", links: "http://covid_tr-acker.surge.sh/", github: "https://github.com/Sijadkhan1678/COVID_19__TRACKER" },
+   { title: "Expense-Tracker", links: "", github:"https://github.com/Sijadkhan1678/Expense_Tracker_App" },
+  { title: "Shoe-Store", links: "http://shoesstore2022.surge.sh/", github:"https://github.com/Sijadkhan1678/Shoe_Store" },
+  { title: "Red-Quen", links: "https://redqueen-2022.surge.sh/", github:"https://github.com/Sijadkhan1678/RedQueen" }, 
+  { title: "Tiny-Short-animation-website", links: "http://ecommorce.surge.sh/", github:"https://github.com/Sijadkhan1678/Animated-Website" }, { title: "", links: "", github },
+  { title: "", links: "", github },{ title: "", links: "", github: "" }
+  ]
   return (
-      <>
+    <>
       <h3>Projects</h3>
-    <ul>
-       
-    </ul>
+      <ul>
+
+      </ul>
     </>
   )
 }
