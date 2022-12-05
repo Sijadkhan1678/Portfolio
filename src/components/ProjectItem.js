10149aaeb98f6d54a67ac889f660f783cbe76d11
sijadkhan1678 <sijadkhan1678@gmail.com>
2022-12-05 09:31:17 +0500

    init commit

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ProjectItem = () => {
+  return (
+    <li>
+        <a href="" target="_blank"></a>
+    </li>
+  )
+}
+
+export default ProjectItem
---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ const Projects = () => {
+  return (
+      <>
+      <h3>Projects</h3>
+    <ul>
+       
+    </ul>
+    </>
+  )
+}
+
+expo
